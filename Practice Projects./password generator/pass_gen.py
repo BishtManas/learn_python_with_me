22d41e0356387de8d1f9f7a57716c85e8ea33d7f
Manas Bisht <bishtmanas2008@gmail.com>
2025-09-03 19:23:21 +0530

    add some space between the code for better interface for user.

--- Practice Projects./password generator/pass_gen.py
+++ Practice Projects./password generator/pass_gen.py
@@ -9,18 +9,25 @@ def generate_password(length, choice):
     elif choice == "both":
         characters = string.ascii_letters + string.digits
     else:
+        print()
         print("Invalid choice! Please enter 'number', 'string', or 'both'.")
+        print()
         return None
+    print()
 
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
 try:
+    print()
     length = int(input("Enter the length of the password: "))
+    print()
     choice = input("What type of password do you want? (number / string / both): ").lower()
+    print()
     
     password = generate_password(length, choice)
     if password:
         print("Your generated password is:", password)
+        print()
 except ValueError:
     print("Please enter a valid number for password length.")
