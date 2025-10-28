185a7e0c9f2414efaaa3c999d393f58e2312fd2b
BishtManas <bishtmanas2008@gmail.com>
2025-10-28 23:35:37 +0530

    type of array in numpy tutorial

---
+++ Numpy Tutorial/types of dimensions/1.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+arr = np.arr(3)# this is zero dimension array.
+
+arr1 = np.arr([3,3,4,5])# this is 1 dimension array.
+
+arr2 = np.arr([[3,3,4,5],[3,3,4,5]])# this is 2 dimension array.
+
+arr3 = np.arr([[[3,3,4,5],[3,3,4,5]],[[3,3,4,5],[3,3,4,5]]])# this is 3 dimension array.
+
+
+print(arr)
+print(arr1)
+print(arr2)
+print(arr3)
