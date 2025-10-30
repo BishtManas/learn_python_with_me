184332fef6524371c117bfdbb1420f611d4ed280
BishtManas <bishtmanas2008@gmail.com>
2025-10-30 22:32:29 +0530

    method for using idendity and unity matrix

---
+++ Numpy Tutorial/types of dimensions/4.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+a = np.eye(2)
+
+print(a)# this is for making idendity matrix 
+
+
+b = np.ones((2,2))
+
+
+print(b)# this is for making all the elements ones like zeros method
+
+
