4ecb3a858f17d4845cd00255ad3d2d1c059be678
BishtManas <bishtmanas2008@gmail.com>
2025-10-31 22:41:01 +0530

    method for slicing in array just like in normal list

---
+++ Numpy Tutorial/types of dimensions/10.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[:4])# this will assume automatically from start and ends with n-1 indexing so in this case [1,2,3,4] will print and this is the output
