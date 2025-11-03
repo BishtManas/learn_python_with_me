7829909fe67a5f041b3484d3e57ad88d9ba15c44
BishtManas <bishtmanas2008@gmail.com>
2025-11-03 19:52:43 +0530

    iterating in 2-D array

---
+++ Numpy Tutorial/umPy Array Iterating/2.py
@@ -0,0 +1,7 @@
+# iterating in 2-D arrays
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for m in arr:
+    print(m)# this will print only the element like in 2-d array one element is 1-d array
