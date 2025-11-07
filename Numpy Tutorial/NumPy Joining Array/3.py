e9948936959f2d45819b484aa321624cb4377e08
BishtManas <bishtmanas2008@gmail.com>
2025-11-07 21:47:14 +0530

    method for joining array stack

---
+++ Numpy Tutorial/NumPy Joining Array/3.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis=1)# Stacking is same as concatenation, the only difference is that stacking is done along a new axis.
+
+
+
+print(arr)
