b60da19dcdba277bb451bb74bad8b849ec3131e5
BishtManas <bishtmanas2008@gmail.com>
2025-11-14 12:44:31 +0530

    move into new one

--- Numpy Tutorial/umPy Array Iterating/Iterating Arrays Using nditer()/1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
-# this is 3 -d array
-
-# by using this method you can directly print all element with the help of nditer()
-
-for m in np.nditer(arr):
-    print(m)# this method print all the element by using the only single loop
