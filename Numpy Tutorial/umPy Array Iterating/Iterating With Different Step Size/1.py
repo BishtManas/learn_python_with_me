92d5d930fa6e507b3b1f33e1a3679985712c0bb9
BishtManas <bishtmanas2008@gmail.com>
2025-11-14 12:44:35 +0530

    move into new one

--- Numpy Tutorial/umPy Array Iterating/Iterating With Different Step Size/1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-
-for x in np.nditer(arr[:, ::2]):# the first one is for row and column and the second one is for number of element present in the list
-  print(x)
