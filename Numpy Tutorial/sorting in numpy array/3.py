42eea45e74b951d8d1714a90915daa4f09461a86
BishtManas <bishtmanas2008@gmail.com>
2025-11-14 12:42:53 +0530

    sorting in 2 dimension array

---
+++ Numpy Tutorial/sorting in numpy array/3.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr3 = np.array([[3, 2, 4], [5, 0, 1]])# this will sort by rows this method sort all element present in rows but the rows will still same.
+
+
+print(np.sort(arr3))
